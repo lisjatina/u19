6f087e96e620c5b50d200c5b6e47d904a632dded
Lidia Belyaeva <lvcaciag@gmail.com>
2021-06-15 11:51:51 +0300

    Activity 12 done

--- src/main/java/jtm/activity12/ArrayFiller.java
+++ src/main/java/jtm/activity12/ArrayFiller.java
@@ -2,6 +2,8 @@ package jtm.activity12;
 
 import java.util.Random;
 
+import static jtm.activity12.ArrayFillerManager.array;
+
 public class ArrayFiller implements Runnable {
 
 	int latency; // required latency time (in miliseconds) to simulate real
@@ -11,23 +13,34 @@ public class ArrayFiller implements Runnable {
 	Random random; // Pseudo-random generator
 
 	public ArrayFiller(int latency, int minValue, int maxValue) {
-		// TODO from this constructor call another constructor with more
+		// from this constructor call another constructor with more
 		// parameters and fill missing
 		// values with fixed literals
+		this(latency,  minValue,  maxValue, 0, array.length -1);
 	}
 
 	public ArrayFiller(int latency, int minValue, int maxValue, int from, int to) {
-		// TODO save passed values to created filler object
-		// Create and initialize pseudo-random generator. See more at:
-		// http://docs.oracle.com/javase/7/docs/api/java/util/Random.html
+	this.latency = latency;
+	this.minValue = minValue;
+	this.maxValue = maxValue;
+	this.to = to;
+	this.from = from;
+	random = new Random();
 	}
 
 	@Override
 	public void run() {
-		// TODO when invoked, put filler to sleep for required amount of latency
+		// when invoked, put filler to sleep for required amount of latency
 		// then fill ArrayFillerManager.array from..to cells with random values
 		// in
 		// minValue..maxValue range
+		try {
+			Thread.sleep(latency);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		for (int i = from; i <=to ; i++) {
+		array[i] = random.nextInt(maxValue - minValue + 1) + minValue;
+		}
 	}
-
 }
--- src/main/java/jtm/activity12/ArrayFillerManager.java
+++ src/main/java/jtm/activity12/ArrayFillerManager.java
@@ -20,29 +20,41 @@ public class ArrayFillerManager {
 	// Note that main() method will not be used in unit tests.
 
 	public static int[] setUp(int arraySize, int latency, int minValue, int maxValue) {
-		// TODO save passed values in prepared structure
+		// save passed values in prepared structure
 		// initialize array with passed size
 		// initialize list of threads
 		// return reference to the initialized array
-		return null;
+		ArrayFillerManager.latency = latency;
+		ArrayFillerManager.maxValue = maxValue;
+		ArrayFillerManager.minValue = minValue;
+		array = new int[arraySize];
+
+		return array;
 	}
 
 	public static void fillStupidly() {
-		// TODO create cycle which creates new ArrayFiller objects
+		// create cycle which creates new ArrayFiller objects
 		// with filling range of only ONE cell at a time (i.e. range from..to is
 		// 0..0, then 1..1, etc.) and invoke .run() method for these objects.
 		// Note, that invocation of .run() method directly executes it in
 		// current (main) thread.
 		// Note that this method emulates, what would happen if you would send
 		// just small portions of data through media with latency.
+		for (int i = 0;  i < array.length; i++) {
+		ArrayFiller filler = new ArrayFiller(latency,minValue, maxValue, i, i);
+		filler.run();
+		}
 	}
 
 	public static void fillSequentially() {
-		// TODO create one ArrayFiller object with filling range for ALL array but
+		// create one ArrayFiller object with filling range for ALL array but
 		// executed just in SINGLE thread by invocation of .run() method directly.
 		// Note that this method emulates, what would happen if you would do
 		// proper "buffering" with large amount of data, but do execution just
 		// in single thread.
+
+		ArrayFiller filler = new ArrayFiller(latency,minValue, maxValue);
+		filler.run();
 	}
 
 	public static void fillParalelly() {
@@ -58,7 +70,27 @@ public class ArrayFillerManager {
 		// actually finished by calling .join() method for them.
 		// Note that this method emulates, what would happen if you do proper
 		// buffering and scaling of the execution.
-
+		int threadNo =4;
+		int range = array.length/threadNo;
+		int from = 0;
+		int to = 0;
+		threads = new LinkedList<>();
+		for (int i = 1; i <= threadNo; i++) {
+			to = to+range;
+			if (to == array.length)
+				to = array.length -1;
+			Thread thread = new Thread(new ArrayFiller(latency,minValue,maxValue,from, to));
+			thread.start();
+			threads.add(thread);
+			from = to +1;
+		}
+		for (Thread thread :threads
+		) {
+			try {
+				thread.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-
 }
