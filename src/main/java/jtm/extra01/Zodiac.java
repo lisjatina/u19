8cd7a38ff4d329664823004f80c9d7c473b4d18c
Lidia Belyaeva <lvcaciag@gmail.com>
2021-06-15 09:08:09 +0300

    Zodiac done

--- src/main/java/jtm/extra01/Zodiac.java
+++ src/main/java/jtm/extra01/Zodiac.java
@@ -19,29 +19,29 @@ public class Zodiac {
 	public static String getZodiac(int day, int month) {
 		String zodiac = null;
 
-		if      ((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 1 && day <= 19))
+		if      ((month == 12 && day >= 22 && day <= 31) || (month ==  1 && day >= 1 && day <= 20))
 			zodiac = "Capricorn";
-		else if ((month ==  1 && day >= 20 && day <= 31) || (month ==  2 && day >= 1 && day <= 18))
+		else if ((month ==  1 && day >= 21 && day <= 31) || (month ==  2 && day >= 1 && day <= 19))
 			zodiac = "Aquarius";
-		else if ((month ==  2 && day >= 19 && day <= 29) || (month ==  3 && day >= 1 && day <= 20))
+		else if ((month ==  2 && day >= 20 && day <= 29) || (month ==  3 && day >= 1 && day <= 20))
 			zodiac = "Pisces";
-		else if ((month ==  3 && day >= 21 && day <= 31) || (month ==  4 && day >= 1 && day <= 19))
+		else if ((month ==  3 && day >= 21 && day <= 31) || (month ==  4 && day >= 1 && day <= 20))
 			zodiac = "Aries";
-		else if ((month ==  4 && day >= 20 && day <= 30) || (month ==  5 && day >= 1 && day <= 20))
+		else if ((month ==  4 && day >= 21 && day <= 30) || (month ==  5 && day >= 1 && day <= 21))
 			zodiac = "Taurus";
-		else if ((month ==  5 && day >= 21 && day <= 31) || (month ==  6 && day >= 1 && day <= 20))
+		else if ((month ==  5 && day >= 22 && day <= 31) || (month ==  6 && day >= 1 && day <= 21))
 			zodiac = "Gemini";
-		else if ((month ==  6 && day >= 21 && day <= 30) || (month ==  7 && day >= 1 && day <= 22))
+		else if ((month ==  6 && day >= 22 && day <= 30) || (month ==  7 && day >= 1 && day <= 22))
 			zodiac = "Cancer";
 		else if ((month ==  7 && day >= 23 && day <= 31) || (month ==  8 && day >= 1 && day <= 22))
 			zodiac = "Leo";
-		else if ((month ==  8 && day >= 23 && day <= 31) || (month ==  9 && day >= 1 && day <= 22))
+		else if ((month ==  8 && day >= 23 && day <= 31) || (month ==  9 && day >= 1 && day <= 23))
 			zodiac = "Virgo";
-		else if ((month ==  9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 22))
+		else if ((month ==  9 && day >= 23 && day <= 30) || (month == 10 && day >= 1 && day <= 23))
 			zodiac = "Libra";
-		else if ((month == 10 && day >= 23 && day <= 31) || (month == 11 && day >= 1 && day <= 21))
+		else if ((month == 10 && day >= 24 && day <= 31) || (month == 11 && day >= 1 && day <= 22))
 			zodiac = "Scorpio";
-		else if ((month == 11 && day >= 22 && day <= 30) || (month == 12 && day >= 1 && day <= 21))
+		else if ((month == 11 && day >= 23 && day <= 30) || (month == 12 && day >= 1 && day <= 21))
 			zodiac = "Sagittarius";
 		return zodiac;
 	}
