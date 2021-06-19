d0ba0f1ab8debae73ed1904871a26d80fc446b58
Lidia Belyaeva <lvcaciag@gmail.com>
2021-06-19 12:10:00 +0300

    Extra 02 first exercise done

--- src/main/java/jtm/extra02/ArrayListMethods.java
+++ src/main/java/jtm/extra02/ArrayListMethods.java
@@ -7,16 +7,25 @@ public class ArrayListMethods {
 	List<Integer> myList = new ArrayList<Integer>();
 
 	public List<Integer> checkArray(int comparator, int... numbers) {
-		// TODO #1:Implement method that compares values of passed array with
+		// #1:Implement method that compares values of passed array with
 		// passed comparator.
 		// Return list with values that are smaller than comparator.
 		// Hint: Investigate how varargs are used.
+
+		for(int number: numbers) {
+			if(number<comparator) {
+				myList.add(number);
+			}
+		}
 		return myList;
 	}
 
 	public int sumResult() {
 		int sum = 0;
-		// TODO #2: Count element sum of the list
+		// #2: Count element sum of the list
+		for (Integer number : myList) {
+			sum += number;
+		}
 		return sum;
 	}
 }
